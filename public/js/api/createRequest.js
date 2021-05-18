3c1e8eb5add21f5e2d5ccb6676a97c539db2945d
dadiakov <74093790+dadiakov@users.noreply.github.com>
2021-05-18 15:48:32 +0300

    Update 18.05

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,33 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+  
+    let xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    let url = options.url;
+    let dataToSend = null;
+  
+    if (options.method === 'GET') {
+      if (options.data) {
+         url = options.url + '?';
+         for (let prop in options.data) {
+            url += prop + '=' + options.data[prop] + '&'
+          }
+         url = url.substr(0, url.length - 1);
+      }
 
-};
+    } else {
+        let dataToSend = new FormData();
+        for (let prop in options.data) {
+          dataToSend.append = (prop, options.data[prop]);
+        }
+    }
+  
+    try {
+        xhr.open(options.method, url);
+        xhr.send(dataToSend);  
+    } catch (err) {
+        options.callback(err); 
+    }
+    xhr.onload = () => { options.callback(null, xhr.response) };
+}
